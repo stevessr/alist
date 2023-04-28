0d17370b288636fa8ee07a27fae6b05233a82350
stevessr <89645372+stevessr@users.noreply.github.com>
2023-04-29 06:38:04 +0800

    lint

---
+++ public/dist/assets/markdown.7b643d02.js
@@ -0,0 +1,2 @@
+import{f as n,a0 as d,o,bF as i}from"./index.bd54b040.js";import{d as s}from"./useUtil.3c653e5c.js";import{M as m}from"./Markdown.a9f378a3.js";import"./api.63549c60.js";const p=()=>{const[t]=s(),a=e=>o.obj.name.endsWith(".md")?e:"```"+i(o.obj.name)+`
+`+e+"\n```";return n(d,{get loading(){return t.loading},get children(){return n(m,{get children(){var e,r;return a((r=(e=t())==null?void 0:e.content)!=null?r:"")}})}})};export{p as default};
---
+++ public/dist/assets/markdown_with_word_wrap.15aa9795.js
@@ -0,0 +1,2 @@
+import{f as o,a0 as s,o as n,bF as d}from"./index.bd54b040.js";import{d as i}from"./useUtil.3c653e5c.js";import{M as m}from"./Markdown.a9f378a3.js";import"./api.63549c60.js";const g=()=>{const[r]=i(),a=e=>n.obj.name.endsWith(".md")?e:"```"+d(n.obj.name)+`
+`+e+"\n```";return o(s,{get loading(){return r.loading},get children(){return o(m,{class:"word-wrap",get children(){var e,t;return a((t=(e=r())==null?void 0:e.content)!=null?t:"")}})}})};export{g as default};
